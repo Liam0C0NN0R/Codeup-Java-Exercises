7965ed03deecc63617d1292a713920e35d32236f
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-30 15:40:24 -0400

    Collections Java exercises, bonus 5, Attendance.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -2,12 +2,13 @@ package grades;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
         HashMap<String, Student> students = createStudents();
-
+        
         System.out.println("Welcome to the Grades Application!");
         System.out.println();
         System.out.println("Here are the GitHub usernames of our students:");
@@ -20,8 +21,10 @@ public class GradesApplication {
         // CLI logic
         Scanner scanner = new Scanner(System.in);
         String answer;
+        System.out.println(students);
 
         do {
+            DecimalFormat decimalFormat = new DecimalFormat("0.00");
             System.out.println("Available Options:");
             System.out.println("1. View student information");
             System.out.println("2. View all grades for all students");
@@ -45,7 +48,7 @@ public class GradesApplication {
                     } else {
                         System.out.println("Name: " + student.getName());
                         System.out.println("GitHub Username: " + usernameInput);
-                        System.out.println("Current Average: " + student.getGradeAverage());
+                        System.out.println("Current Average: " + decimalFormat.format(student.getGradeAverage()));
                     }
                 }
                 case 2 -> {
@@ -86,24 +89,36 @@ public class GradesApplication {
         student1.addGrade(85);
         student1.addGrade(90);
         student1.addGrade(92);
+        student1.recordAttendance("2022-05-10", "A");
+        student1.recordAttendance("2022-05-11", "A");
+        student1.recordAttendance("2022-05-12", "P");
         students.put("Versatile33", student1);
 
         Student student2 = new Student("Lewis Hamilton");
         student2.addGrade(75);
         student2.addGrade(80);
         student2.addGrade(88);
+        student2.recordAttendance("2022-05-10", "P");
+        student2.recordAttendance("2022-05-11", "A");
+        student2.recordAttendance("2022-05-12", "A");
         students.put("GoingHam44", student2);
 
         Student student3 = new Student("Michael Schumacher");
         student3.addGrade(95);
         student3.addGrade(87);
         student3.addGrade(91);
+        student3.recordAttendance("2022-05-10", "P");
+        student3.recordAttendance("2022-05-11", "A");
+        student3.recordAttendance("2022-05-12", "P");
         students.put("Schu_in47", student3);
 
         Student student4 = new Student("Ayrton Senna");
         student4.addGrade(80);
         student4.addGrade(88);
         student4.addGrade(92);
+        student4.recordAttendance("2022-05-10", "P");
+        student4.recordAttendance("2022-05-11", "P");
+        student4.recordAttendance("2022-05-12", "P");
         students.put("SennaSenpai27", student4);
 
         return students;
@@ -126,16 +141,20 @@ public class GradesApplication {
     }
 
     private static void generateCSVReport(HashMap<String, Student> students) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
         System.out.println("CSV Report:");
-        System.out.println("name,github_username,average");
+        System.out.println("Name, Github username, GPA");
 
-        for (Map.Entry<String, Student> entry : students.entrySet()) {
-            String username = entry.getKey();
-            Student student = entry.getValue();
+        for (String key : students.keySet()) {
+            Student student = students.get(key);
             String name = student.getName();
-            double average = student.getGradeAverage();
-
-            System.out.println(name + "," + username + "," + average);
+            double average = Double.parseDouble(decimalFormat.format(student.getGradeAverage()));
+            System.out.println(name + ", " + key + ", " + average);
+            System.out.println("Grades: " + student.getGrades());
+            System.out.println("Attendance: " + student.getAttendance());
+            System.out.println("Attendance Percentage: " + decimalFormat.format(student.getAttendancePercentage()));
+            System.out.println("Absent Dates: " + student.getAbsentDates());
+            System.out.println();
         }
     }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,13 +1,20 @@
 package grades;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Student {
     private String name;
     private ArrayList<Integer> grades;
+    private HashMap<String, String> attendance;
 
     public Student(String name) {
         this.name = name;
         this.grades = new ArrayList<>();
+        this.attendance = new HashMap<>();
     }
+
     public String getName() {
         return name;
     }
@@ -15,6 +22,7 @@ public class Student {
     public void addGrade(int grade) {
         grades.add(grade);
     }
+
     public ArrayList<Integer> getGrades() {
         return grades;
     }
@@ -30,5 +38,39 @@ public class Student {
         }
         return (double) sum / grades.size();
     }
-}
 
+    public void recordAttendance(String date, String value) {
+        if (value.equalsIgnoreCase("A") || value.equalsIgnoreCase("P")) {
+            attendance.put(date, value);
+        } else {
+            System.out.println("Invalid attendance value. Please enter 'A' for Absent or 'P' for Present.");
+        }
+    }
+
+    public double getAttendancePercentage() {
+        int totalDays = attendance.size();
+        int absences = 0;
+        for (String value : attendance.values()) {
+            if (value.equalsIgnoreCase("A")) {
+                absences++;
+            }
+        }
+        return (double) (totalDays - absences) / totalDays * 100;
+    }
+
+    public List<String> getAbsentDates() {
+        List<String> absentDates = new ArrayList<>();
+        for (Map.Entry<String, String> entry : attendance.entrySet()) {
+            String date = entry.getKey();
+            String value = entry.getValue();
+            if (value.equalsIgnoreCase("A")) {
+                absentDates.add(date);
+            }
+        }
+        return absentDates;
+    }
+
+    public HashMap<String, String> getAttendance() {
+        return attendance;
+    }
+}
